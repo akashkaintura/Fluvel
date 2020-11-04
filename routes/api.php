85affcf419b1ede88eb7e6ee03427a527560b6a1
Cole.Akash <cole.akash@Akash.local>
2020-11-04 13:02:12 +0530

    APIs in Laravel

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::post('/login', 'UsersController@login');
+    Route::post('/register', 'UsersController@register');
+    Route::get('/logout', 'UsersController@logout')->middleware('auth:api');
+});
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
